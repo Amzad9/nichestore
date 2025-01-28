fd02d58110596f19ebef7f2a031b2c87a68258e0
amzad949 <amzad.khan@ftachdata.com>
2025-01-28 15:44:10 +0530

    cors issue

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://jewelry-seven.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With' ,'withCredentials']
